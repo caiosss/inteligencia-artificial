b70723bd65259b4abef1f41123bd1a49236ecacd
João Pedro <jotamendes944@gmail.com>
2025-09-03 14:47:02 -0300

    Começando o projeto

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello, World!")
+import numpy as np
+
+array_teste = np.array([[1,2,3],
+                       [4,5,6],
+                       [7,8,9]])
+
+print(array_teste.shape)
+print(array_teste.size)
+print(array_teste[:,1]) # INDEXAÇÃO estou dizendo que não quero pegar nenhuma linha e somente a coluna 1
+print(array_teste[1,:]) # INDEXAÇÃo pego só a linha 1 e nenhuma coluna
